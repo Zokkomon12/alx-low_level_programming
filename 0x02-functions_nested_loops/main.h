9a1b6d7d091ccd29bebe6c3fa4ce2d30ab86fedc
Zokkomon12 <ayenioladimeji60@gmail.com>
2022-09-13 11:21:23 -0700

    task 0 to 6

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,52 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+* _putchar - writes the character c to std
+* c: parameter to compare
+* Return: 1 or 0
+*/
+
+int _putchar(char c);
+ /**
+ * int - a data type to specify the kind of value that is used in the function
+ * _putchar - the "_" before the putchar is used when declaring a fuction
+ * (char *c) - is the arguments passed
+ */
+void print_alphabet(void);
+ /**
+ * void - used to return nothing
+ * print_alphabet - used when declaring a fuction
+ * for alphabets
+ */
+ void print_alphabet_x10(void);
+ /**
+ * void - used to return nothing
+ * print_alphabet - used to print
+ * for alphabets
+ */
+ int _islower(int c);
+ /**
+ * void - used to return nothing
+ * used to print
+ * for 
+ */
+ int _isalpha(int c);
+ /**
+ * void - used to return nothing
+ * used to print
+ * for 
+ */
+ int print_sign(int n);
+ /**
+ * void - used to return nothing
+ * used to print
+ * for 
+ */
+ int _abs(int);
+ /**
+ * void - used to return nothing
+ * used to print
+ * for 
+ */
+#endif
