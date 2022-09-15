32a57dbaf8c10f6fbd530b8f6dfcf9eb94f4ff9a
Zokkomon12 <ayenioladimeji60@gmail.com>
2022-09-15 06:43:16 -0700

    advanced task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -49,4 +49,15 @@ void print_alphabet(void);
  * used to print
  * for 
  */
+int print_last_digit(int a);
+/**
+ * int - an integer data type
+ * 
+ *
+ */
+ void jack_bauer(void);
+ void times_table(void);
+ int add(int, int);
+ void print_to_98(int n);
+ void print_times_table(int n);
 #endif
